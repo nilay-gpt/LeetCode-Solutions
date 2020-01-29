526d95991eaa522798f66a6f24b06db07bd4294f
nilay <nilaygpt@gmail.com>
2020-01-29 21:32:51 +0530

    Create find_substring.py

---
+++ find_substring.py
@@ -0,0 +1,38 @@
+"""
+Implement strStr().
+
+Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+Example 1:
+
+Input: haystack = "hello", needle = "ll"
+Output: 2
+Example 2:
+
+Input: haystack = "aaaaa", needle = "bba"
+Output: -1
+Clarification:
+
+What should we return when needle is an empty string? This is a great question to ask during an interview.
+
+For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
+"""
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if needle == "":
+            print 0
+        if needle in haystack:
+            value = haystack.index(list(needle)[0])
+            print value
+        else:
+            print -1
+        return haystack.find(needle)
+
+if __name__ == "__main__":
+    a = Solution()
+    a.strStr("hello", "llb")
