0a2ed8558d83433583e6287bd18bc475ab250527
nilay <nilaygpt@gmail.com>
2020-02-20 20:04:32 +0530

    Update patterns.py

--- patterns.py
+++ patterns.py
@@ -100,21 +100,46 @@ from __future__ import print_function
 **
 """
 
-def make_pattern(flag_width, flag_height, pole_height):
-	pole_width = 2
-	for i in range(0, flag_height):
-		for j in range(0, flag_width):
-			print("*", end="")
-		print("\r")
-	for i in range(0, pole_height):
-		for j in range(0, pole_width):
-			print("*", end="")
-		print("\r")
+# def make_pattern(flag_width, flag_height, pole_height):
+# 	pole_width = 2
+# 	for i in range(0, flag_height):
+# 		for j in range(0, flag_width):
+# 			print("*", end="")
+# 		print("\r")
+# 	for i in range(0, pole_height):
+# 		for j in range(0, pole_width):
+# 			print("*", end="")
+# 		print("\r")
 
 
 
-if __name__ == "__main__":
-	flag_width = 18
-	flag_height = 4
-	pole_height = 5
-	make_pattern(flag_width, flag_height, pole_height)
+# if __name__ == "__main__":
+# 	flag_width = 18
+# 	flag_height = 4
+# 	pole_height = 5
+# 	make_pattern(flag_width, flag_height, pole_height)
+
+"""
+1 
+1 1 
+1 2 1 
+1 3 3 1 
+1 4 6 4 1 
+"""
+def printPascal(n):  
+  
+    for line in range(1, n + 1):  
+        C = 1; # used to represent C(line, i)  
+        for i in range(1, line + 1):  
+              
+            # The first value in a  
+            # line is always 1  
+            print(C, end = " ");  
+            C = int(C * (line - i) / i);
+            print ("---->", C, line, i)
+        print("");  
+  
+# Driver code  
+n = 5;  
+printPascal(n); 
+  
