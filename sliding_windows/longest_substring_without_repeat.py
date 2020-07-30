3e49a8ee736c84e8d3bc6a3bd040a3127b697f20
nilay <nilaygpt@gmail.com>
2020-07-30 21:03:17 +0530

    Create longest_substring_without_repeat.py

---
+++ sliding_windows/longest_substring_without_repeat.py
@@ -0,0 +1,24 @@
+"""
+Input: "abcabcbb"
+Output: 3 
+Explanation: The answer is "abc", with the length of 3. 
+
+https://leetcode.com/problems/longest-substring-without-repeating-characters/
+"""
+
+class Solution:
+    def lengthOfLongestSubstring(self, s):
+        
+        if len(s) == 0:
+            return 0
+        map = {}
+        max_length = start = 0
+        
+        for i in range(len(s)):
+            if s[i] in map and start <= map[s[i]]:
+                start = map[s[i]] + 1
+                
+            else:
+                max_length = max(max_length, i - start + 1)
+            map[s[i]] = i
+        return(max_length)
