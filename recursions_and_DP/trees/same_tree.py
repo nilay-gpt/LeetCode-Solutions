e32549ee08850d910df74a6d333c970812f1b8f8
nilay <nilaygpt@gmail.com>
2020-08-21 16:31:01 +0530

    Update same_tree.py

--- recursions_and_DP/trees/same_tree.py
+++ recursions_and_DP/trees/same_tree.py
@@ -1,21 +1,3 @@
-"""
-Given two binary trees, write a function to check if they are the same or not.
-
-Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
-
-Example 1:
-
-Input:     1         1
-          / \       / \
-         2   3     2   3
-
-        [1,2,3],   [1,2,3]
-
-Output: true
-
-https://leetcode.com/problems/same-tree/
-"""
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -26,41 +8,37 @@ from collections import deque
 
 class Solution:
     def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
-        # if not (p or q): return True
-        # if not (p and q): return False
-        # queue = deque([(p, q)])
-        # while queue:
-        #     n = len(queue)
-        #     for _ in range(n):
-        #         ln, rn = queue.popleft()
-        #         if not ln.val == rn.val:
-        #             return False
-        #         if ln 
         
-        # BFS approch
-        if not (p or q): return True
-        if not (p and q): return False
-        return self.return_node_list(p) == self.return_node_list(q)
+        # early return approach approch
+        if p and q:
+            return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        
+        return p is q
+        
+        # BFS approch, mentain output for both the tree and compare the output
+#         if not (p or q): return True
+#         if not (p and q): return False
+#         return self.return_node_list(p) == self.return_node_list(q)
         
         
-    def return_node_list(self, root):
-        output = []
-        queue = deque([root])
+#     def return_node_list(self, root):
+#         output = []
+#         queue = deque([root])
 
-        while queue:
-            for _ in range(0, len(queue)):
-                node = queue.popleft()
-                output.append(node.val)
+#         while queue:
+#             for _ in range(0, len(queue)):
+#                 node = queue.popleft()
+#                 output.append(node.val)
 
-                if node.left:
-                    queue.append(node.left)
-                    output.append(node.left.val)
-                else: output.append(None)
-                if node.right:
-                    queue.append(node.right)
-                    output.append(node.right.val)
-                else: output.append(None)
+#                 if node.left:
+#                     queue.append(node.left)
+#                     output.append(node.left.val)
+#                 else: output.append(None)
+#                 if node.right:
+#                     queue.append(node.right)
+#                     output.append(node.right.val)
+#                 else: output.append(None)
                 
-        return output
+#         return output
 
         
