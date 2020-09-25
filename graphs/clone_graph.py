c795b200c8e200a5fa717c74d68da56e1a6202ec
nilay <nilaygpt@gmail.com>
2020-09-25 16:37:37 +0530

    Create clone_graph.py

---
+++ graphs/clone_graph.py
@@ -0,0 +1,61 @@
+"""
+Given a reference of a node in a connected undirected graph.
+
+Return a deep copy (clone) of the graph.
+
+Each node in the graph contains a val (int) and a list (List[Node]) of its neighbors.
+
+class Node {
+    public int val;
+    public List<Node> neighbors;
+}
+ 
+
+Test case format:
+
+For simplicity sake, each node's value is the same as the node's index (1-indexed). For example, the first node with val = 1, the second node with val = 2, and so on. The graph is represented in the test case using an adjacency list.
+
+Adjacency list is a collection of unordered lists used to represent a finite graph. Each list describes the set of neighbors of a node in the graph.
+
+The given node will always be the first node with val = 1. You must return the copy of the given node as a reference to the cloned graph.
+
+Example 1:
+Input: adjList = [[2,4],[1,3],[2,4],[1,3]]
+Output: [[2,4],[1,3],[2,4],[1,3]]
+Explanation: There are 4 nodes in the graph.
+1st node (val = 1)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+2nd node (val = 2)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
+3rd node (val = 3)'s neighbors are 2nd node (val = 2) and 4th node (val = 4).
+4th node (val = 4)'s neighbors are 1st node (val = 1) and 3rd node (val = 3).
+
+https://leetcode.com/problems/clone-graph/
+"""
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+from collections import deque
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        """
+        BFS queue.
+        """
+        if not node: return
+
+        clone_dict = {node: Node(node.val)}
+        queue = deque([node])
+        
+        while queue:
+            curr_node = queue.popleft()
+            for nei in curr_node.neighbors:
+                if nei not in clone_dict:
+                    # store copy of the neighboring node
+                    clone_dict[nei] = Node(nei.val)
+                    queue.append(nei)
+                # connect the node copy at hand to its neighboring nodes (also copies) -------- [1]
+                clone_dict[curr_node].neighbors.append(clone_dict[nei])
+        return clone_dict[node]
